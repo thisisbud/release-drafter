c2056c722f46d64af9b56dfdb5c78df20f39364e
Sam Sully <sam.sully@thisisbud.com>
2022-08-10 09:39:26 +0100

    show diff

--- .github/no-unstaged-files.sh
+++ .github/no-unstaged-files.sh
@@ -2,6 +2,7 @@
 
 if [[ "$(git status --porcelain)" != "" ]]; then
   git status
+  git diff
   echo "::error::💥 Unstaged changes detected. Locally try running: yarn prettier && yarn lint --fix && yarn build"
   exit 1
 fi
